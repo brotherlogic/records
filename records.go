6bf74a1e296a27a4574dbb495534fc50a3a936fc
Simon Tucker <brotherlogic@gmail.com>
2016-08-07 14:51:55 -0700

    First pass

---
+++ records.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+
+	pb "github.com/brotherlogic/discogssyncer/server"
+	pbdi "github.com/brotherlogic/discovery/proto"
+	pbd "github.com/brotherlogic/godiscogs"
+	pbo "github.com/brotherlogic/recordsorganiser/proto"
+)
+
+func getIP(servername string, ip string, port int) (string, int) {
+	conn, _ := grpc.Dial(ip+":"+strconv.Itoa(port), grpc.WithInsecure())
+	defer conn.Close()
+
+	registry := pbdi.NewDiscoveryServiceClient(conn)
+	entry := pbdi.RegistryEntry{Name: servername}
+	r, _ := registry.Discover(context.Background(), &entry)
+	return r.Ip, int(r.Port)
+}
+
+func addLocation(name string, units int, folders string) {
+	location := &pbo.Location{Name: name, Units: int32(units)}
+	for _, folder := range strings.Split(folders, ",") {
+		folderID, _ := strconv.Atoi(folder)
+		location.FolderIds = append(location.FolderIds, int32(folderID))
+	}
+
+	//Move the previous record down to uncategorized
+	dServer, dPort := getIP("recordsorganiser", "10.0.1.17", 50055)
+	dConn, err := grpc.Dial(dServer+":"+strconv.Itoa(dPort), grpc.WithInsecure())
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer dConn.Close()
+	dClient := pbo.NewOrganiserServiceClient(dConn)
+	newLocation, _ := dClient.AddLocation(context.Background(), location)
+	log.Printf("New Location = %v", newLocation)
+}
+
+func addRecord(id int) {
+	dServer, dPort := getIP("discogssyncer", "10.0.1.17", 50055)
+
+	//Move the previous record down to uncategorized
+	dConn, err := grpc.Dial(dServer+":"+strconv.Itoa(dPort), grpc.WithInsecure())
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer dConn.Close()
+	dClient := pb.NewDiscogsServiceClient(dConn)
+
+	release := &pbd.Release{Id: int32(id)}
+	folderAdd := &pb.ReleaseMove{Release: release, NewFolderId: int32(821802)}
+	_, err = dClient.AddToFolder(context.Background(), folderAdd)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	addFlags := flag.NewFlagSet("AddRecord", flag.ExitOnError)
+	var id = addFlags.Int("id", 0, "ID of record to add")
+
+	addLocationFlags := flag.NewFlagSet("AddLocation", flag.ExitOnError)
+	var name = addLocationFlags.String("name", "", "The name of the new location")
+	var units = addLocationFlags.Int("units", 0, "The number of units in the location")
+	var folderIds = addLocationFlags.String("folders", "", "The list of folder IDs")
+
+	switch os.Args[1] {
+	case "add":
+		if err := addFlags.Parse(os.Args[2:]); err == nil {
+			addRecord(*id)
+		}
+	case "addlocation":
+		if err := addLocationFlags.Parse(os.Args[2:]); err == nil {
+			addLocation(*name, *units, *folderIds)
+		}
+	}
+}
